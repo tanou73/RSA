c075d7e961aa7de611ddf9efcf4d0fb435490b96
Maxime <maxime.layat@gmail.com>
2014-02-19 18:55:34 +0100

    refactor gitignore + debut chiffre

--- RSA/chiffre.cxx
+++ RSA/chiffre.cxx
@@ -1,9 +1,27 @@
 #include <iostream>
 #include <gmpxx.h>
+#include <cstdlib> 
 
 using namespace std;
 
-int main( int argc, char ** argv )
+int main( int argc, char * argv [] )
 {
-  cout << "** CHIFFRE **" << endl;
+	string message;
+	getline(cin, message);
+
+	if (argc != 4)
+	{
+		cout << "Nombre d'arguments incorrect ( [n] [b] [t] ) " << endl;;
+		exit(1);
+	}
+
+	mpz_class n = atoi(argv[1]);
+	mpz_class b = atoi(argv[2]);
+	mpz_class t = atoi(argv[3]);
+
+	cout << "** CHIFFRE sur " << t  << " bits ***" << endl;
+
+	cout << "Clé publique utilisée  : (" << n << "," << b << ") "  << endl;
+	cout << "Message : " << message << endl;
+
 }
