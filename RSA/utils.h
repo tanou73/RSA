637706448feb4bd9a93f6eaf857cf3de6bdf7c5c
hugo <hugo.chiavenuto@gmail.com>
2014-02-19 18:25:23 +0100

    gencle (generateur de cle) marche

--- RSA/utils.h
+++ RSA/utils.h
@@ -11,8 +11,12 @@ class Utils
 
 	// public
 	public:
-		static mpz_class getNombrePremier(gmp_randclass& seeder);
+		static mpz_class genereNombrePremier(gmp_randclass& seeder);
+		static mpz_class genereNombrePremier(gmp_randclass& seeder, mpz_class& M);
+		static mpz_class pgcd(mpz_class& a, mpz_class& b);
 		static bool estPremierLent(mpz_class& nb);
+		static bool sontPremier(mpz_class a, mpz_class b);
+
 };
 
 #endif // UTILS_H
