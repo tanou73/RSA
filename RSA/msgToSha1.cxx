72494a8a6638bed9a3ab370e62a152a615b5b3c2
hugo <hugo.chiavenuto@gmail.com>
2014-02-20 18:22:20 +0100

    lol

---
+++ RSA/msgToSha1.cxx
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <gmpxx.h>
+#include <cstdlib>
+#include <sstream>
+#include "utils.h"
+
+using namespace std;
+// On signe avec notre clé privée, comme ça le destinataire déchiffrera avec notre clé publique
+int main( int argc, char * argv [] )
+{
+	//const char * fileName =  (argc == 2 ) ? argv[1] : "~/.my_rsa";
+
+	string tmp = "";
+	string msg = "";
+
+	do 
+	{
+		msg += tmp + "\n";
+	} while (getline(cin, tmp));
+
+	msg += tmp;
+
+	string encrypted = Utils::encryptSha1(msg);
+
+	cout << encrypted;
+}
